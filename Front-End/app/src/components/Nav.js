7d82158b2e8f43a88d9eff3da59f10b156c12bf6
sM0kfyz <ivansharapenkov@gmail.com>
2018-07-19 16:11:34 +0300

    Create Nav and Work components

---
+++ Front-End/app/src/components/Nav.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Nav extends Component {
+    render() {
+        return (<p>Nav panel</p>)
+    }
+}
+
+export default Nav
---
+++ Front-End/app/src/components/Work.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class Work extends Component {
+    render() {
+        return (<p>Work Component</p>)
+    }
+}
+
+export default Work
