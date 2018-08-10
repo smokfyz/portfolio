b7868a933a26c315f0dc135d7e8ee323c7bf0ba1
sM0kfyz <ivansharapenkov@gmail.com>
2018-08-10 12:33:52 +0300

    Create contact component

--- Front-End/app/src/components/contact/Contact.js
+++ Front-End/app/src/components/contact/Contact.js
@@ -7,8 +7,30 @@ class Contact extends Component {
             <div className="contact">
                 <h1>Contact Me</h1>
                 <div className="contacts">
+                    <ul>
+                        <li><i className="icon i-email" />ivansharapenkov@gmail.com</li>
+                        <li><i className="icon i-skype" />smokfyz</li>
+                        <li><i className="icon i-vk" />vk.com/ivansharapenkov</li>
+                    </ul>
                 </div>
                 <div className="send-email">
+                    <form>
+                        <label>
+                            Name
+                            <input type="text" name="name" />
+                        </label>
+                        <label>
+                            Email
+                            <input type="text" name="email" />
+                        </label>
+                        <label>
+                            Message
+                            <textarea name="message" />
+                        </label>
+                        <label>
+                            <input type="submit" value="SEND" />
+                        </label>
+                    </form>
                 </div>
             </div>
         )
